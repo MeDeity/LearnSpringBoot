9d5acf425ed3b23c824da751027d7508711cc435
MeDeity <langrenbule@gmail.com>
2019-09-26 22:28:49 +0800

    mybatis的使用demo

---
+++ learn-spring-mybatis/src/main/java/com/example/learnspringmybatis/LearnSpringMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspringmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringMybatisApplication.class, args);
+    }
+
+}
